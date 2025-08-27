d1a4ab19c8217aba7de8ecbf929387c8490e9c7e
m.trofimov <m.trofimov@zebrains.team>
2025-08-27 20:01:26 +0700

    update package.json

--- packages/python/src/renderers/index.ts
+++
@@ -1,6 +0,0 @@
-import { HtmlRenderer } from './htmlRenderer.js';
-import { JinjaRenderer } from './jinjaRenderer.js';
-export const renderers = {
-  jinja: new JinjaRenderer(),
-  html: new HtmlRenderer(),
-} as const;
